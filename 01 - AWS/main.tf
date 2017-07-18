25868c47b93dd255e4130c18a897fd495d84278c
Stan <stanislav.siska@yahoo.com>
2017-07-18 22:40:32 +0200

    Redirect from 8080 to 80 - dont work, testing 80

--- 01 - AWS/main.tf
+++ 01 - AWS/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_security_group" "instance" {
   name = "terraform-secgroup-instance"
   ingress {
-    from_port = 8080
+    from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
