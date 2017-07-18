0557c0b8c77a45471ce82bc362bb5965c03ac748
Stan <stanislav.siska@yahoo.com>
2017-07-18 21:41:12 +0200

    Adding tags

--- 01 - AWS/main.tf
+++ 01 - AWS/main.tf
@@ -1,5 +1,6 @@
 # 1. Generate credentials for AWS CLI
-# 2. cmd: terraform plan for verification
+# 2. cmd: terraform plan -- verification/sanity check, similar to diff
+# 3. cmd: terraform apply - creates instance
 
 provider "aws" {
   region = "eu-central-1"
@@ -8,4 +9,9 @@ provider "aws" {
 resource "aws_instance" "terraform_first_server" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+}
+
+tags {
+  Name = "terraform_ubuntu_16-04"
+  CostCenter = "None"
 }
