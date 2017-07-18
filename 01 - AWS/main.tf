3644227c414f2d3b74c28fca969af7e5badc4b71
Stan <stanislav.siska@yahoo.com>
2017-07-18 21:37:27 +0200

    Better name for aws instance

--- 01 - AWS/main.tf
+++ 01 - AWS/main.tf
@@ -1,8 +1,11 @@
+# 1. Generate credentials for AWS CLI
+# 2. cmd: terraform plan for verification
+
 provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "first_server" {
+resource "aws_instance" "terraform_first_server" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
 }
