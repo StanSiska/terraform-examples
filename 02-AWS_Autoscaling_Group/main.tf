f39a272b41940350520672c01e41362248a50e4e
Stan <stanislav.siska@yahoo.com>
2017-07-21 13:19:14 +0200

    egress fixing error:
    * aws_security_group.terraform_elb_instance: from_port (80) and to_port (80) must both be 0 to use the 'ALL' "-1" protocol!

--- 02-AWS_Autoscaling_Group/main.tf
+++ 02-AWS_Autoscaling_Group/main.tf
@@ -45,8 +45,8 @@ resource "aws_security_group" "terraform_elb_instance" {
   }
 
   egress {
-    from_port = 80
-    to_port   = 80
+    from_port = 0
+    to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
